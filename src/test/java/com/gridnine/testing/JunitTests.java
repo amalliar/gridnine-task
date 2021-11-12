8b8f6acfc98a25573cff6b3ee1d03be82e36d71a
Alexander Chetverkin <virtual-ins@mail.ru>
2021-11-12 04:47:09 +0300

    tests(JunitTests): full code coverage

--- src/test/java/com/gridnine/testing/JunitTests.java
+++ src/test/java/com/gridnine/testing/JunitTests.java
@@ -3,15 +3,17 @@ package com.gridnine.testing;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class JunitTests {
-    List<Flight> flights = FlightBuilder.createFlights();
+    final List<Flight> flights = FlightBuilder.createFlights();
 
     @Test
     @DisplayName("testDepartsAfterCurrentTime")
@@ -55,5 +57,45 @@ public class JunitTests {
                         FilterBuilder.createFilter("groundTimeLessOrEqualsTwoHours")
                         ));
         assertThat(expected, is(actual));
+    }
+
+    @Test
+    @DisplayName("testEmptyFilterList")
+    void testEmptyFilterList() {
+        List<Flight> expected = new LinkedList<>(flights);
+        List<Flight> actual = FlightFilter.apply(flights, List.of());
+        assertThat(expected, is(actual));
+    }
+
+    @Test
+    @DisplayName("testEmptyFlightList")
+    void testEmptyFlightList() {
+        List<Flight> expected = List.of();
+        List<Flight> actual = FlightFilter.apply(List.of(), List.of());
+        assertThat(expected, is(actual));
+    }
+
+    @Test
+    @DisplayName("testNullFlightList")
+    void testNullFlightList() {
+        Exception ex = Assertions.assertThrows(NullPointerException.class,
+                () -> FlightFilter.apply(null, List.of()));
+        assertThat("parameters can not be null", is(ex.getMessage()));
+    }
+
+    @Test
+    @DisplayName("testNullFilter")
+    void testNullFilter() {
+        Exception ex = Assertions.assertThrows(NullPointerException.class,
+                () -> FlightFilter.apply(flights, (Predicate<Flight>) null));
+        assertThat("parameters can not be null", is(ex.getMessage()));
+    }
+
+    @Test
+    @DisplayName("testUnknownFilter")
+    void testUnknownFilter() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> FilterBuilder.createFilter("random"));
+        assertThat("unknown filter: random", is(ex.getMessage()));
     }
 }
